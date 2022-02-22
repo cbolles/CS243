9904d8e944264e6d28a9808d7e4942e30d530800
cbolles <collinbolles@gmail.com>
2022-02-22 16:13:24 -0500

    Add in array list so far

---
+++ src/spring-2215/week07/array_list.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct list_s {
+    int *contents;
+    size_t length;
+    size_t capacity;
+};
+
+typedef struct list_s List;
+
+List *new_list() {
+    List *new_list = malloc(sizeof(List));
+
+    new_list->contents = malloc(5 * sizeof(int));
+    new_list->length = 0;
+    new_list->capacity = 5;
+
+    return new_list;
+}
+
+int list_get(List *self, size_t index) {
+
+    return self->contents[index];
+}
+
+void list_append(List *self, int new_elem) {
+    // Space is in array
+    if(self->length >= self->capacity) {
+        self->contents = realloc(self->contents, self->capacity * 2 * sizeof(int));
+        self->capacity *= 2;
+    }
+    self->contents[self->length] = new_elem;
+    self->length++;
+}
+
+int main() {
+
+    List *lst = new_list();
+    list_append(lst, 1);
+    list_append(lst, 2);
+    list_append(lst, 3);
+
+    for(int i = 0; i < 3; i++) {
+        printf("lst[%d] = %d\n", i,list_get(lst, i));
+    }
+
+    return 0;
+}
+
